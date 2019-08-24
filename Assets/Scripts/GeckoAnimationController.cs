acc16043757215aced6a38871fa2e4c06a86611e
Crocsx <crocsx.gaming@gmail.com>
2019-08-25 00:31:47 +0900

    Finish Demo

--- Assets/Scripts/GeckoAnimationController.cs
+++ Assets/Scripts/GeckoAnimationController.cs
@@ -19,10 +19,135 @@ public class GeckoAnimationController : MonoBehaviour
     [SerializeField] float rightEyeMaxYRotation;
     [SerializeField] float rightEyeMinYRotation;
 
+    [SerializeField] GeckoLegStepper frontLeftLegStepper;
+    [SerializeField] GeckoLegStepper frontRightLegStepper;
+    [SerializeField] GeckoLegStepper backLeftLegStepper;
+    [SerializeField] GeckoLegStepper backRightLegStepper;
+
+    // How fast we can turn and move full throttle
+    [SerializeField] float turnSpeed;
+    [SerializeField] float moveSpeed;
+    // How fast we will reach the above speeds
+    [SerializeField] float turnAcceleration;
+    [SerializeField] float moveAcceleration;
+    // Try to stay in this range from the target
+    [SerializeField] float minDistToTarget;
+    [SerializeField] float maxDistToTarget;
+    // If we are above this angle from the target, start turning
+    [SerializeField] float maxAngToTarget;
+
+    // World space velocity
+    Vector3 currentVelocity;
+    // We are only doing a rotation around the up axis, so we only use a float here
+    float currentAngularVelocity;
+
+    void Awake()
+    {
+        StartCoroutine(LegUpdateCoroutine());
+    }
+
     void LateUpdate()
     {
+        RootMotionUpdate();
         HeadTrackingUpdate();
         EyesTrackingUpdate();
+    }
+
+    // Only allow diagonal leg pairs to step together
+    IEnumerator LegUpdateCoroutine()
+    {
+        // Run forever
+        while (true)
+        {
+            // Try moving one diagonal pair of legs
+            do
+            {
+                frontLeftLegStepper.RequestMove();
+                backRightLegStepper.RequestMove();
+                // Wait a frame
+                yield return null;
+            
+            // Stay in this loop while either leg is moving.
+            // If only one leg in the pair is moving, the calls to TryMove() will let
+            // the other leg move if it wants to.
+            } while (backRightLegStepper.isMoving || frontLeftLegStepper.isMoving);
+
+            // Do the same thing for the other pair
+            do
+            {
+                frontRightLegStepper.RequestMove();
+                backLeftLegStepper.RequestMove();
+                yield return null;
+            } while (backLeftLegStepper.isMoving || frontRightLegStepper.isMoving);
+        }
+    }
+
+    void RootMotionUpdate()
+    {
+        // Get the direction toward our target
+        Vector3 towardTarget = target.position - transform.position;
+        // Vector toward target on the local XZ plane
+        Vector3 towardTargetProjected = Vector3.ProjectOnPlane(towardTarget, transform.up);
+        // Get the angle from the gecko's forward direction to the direction toward toward our target
+        // Here we get the signed angle around the up vector so we know which direction to turn in
+        float angToTarget = Vector3.SignedAngle(transform.forward, towardTargetProjected, transform.up);
+
+        float targetAngularVelocity = 0;
+
+        // If we are within the max angle (i.e. approximately facing the target)
+        // leave the target angular velocity at zero
+        if (Mathf.Abs(angToTarget) > maxAngToTarget)
+        {
+            // Angles in Unity are clockwise, so a positive angle here means to our right
+            if (angToTarget > 0)
+            {
+                targetAngularVelocity = turnSpeed;
+            }
+            // Invert angular speed if target is to our left
+            else
+            {
+                targetAngularVelocity = -turnSpeed;
+            }
+        }
+
+        // Use our smoothing function to gradually change the velocity
+        currentAngularVelocity = Mathf.Lerp(
+            currentAngularVelocity,
+            targetAngularVelocity,
+            1 - Mathf.Exp(-turnAcceleration * Time.deltaTime)
+        );
+        
+        // Rotate the transform around the Y axis in world space, 
+        // making sure to multiply by delta time to get a consistent angular velocity
+        transform.Rotate(0, Time.deltaTime * currentAngularVelocity, 0, Space.World);
+
+        Vector3 targetVelocity = Vector3.zero;
+
+        // Don't move if we're facing away from the target, just rotate in place
+        if (Mathf.Abs(angToTarget) < 90)
+        {
+            float distToTarget = Vector3.Distance(transform.position, target.position);
+
+            // If we're too far away, approach the target
+            if (distToTarget > maxDistToTarget)
+            {
+                targetVelocity = moveSpeed * towardTargetProjected.normalized;
+            }
+            // If we're too close, reverse the direction and move away
+            else if (distToTarget < minDistToTarget)
+            {
+                targetVelocity = moveSpeed * -towardTargetProjected.normalized;
+            }
+        }
+
+        currentVelocity = Vector3.Lerp(
+            currentVelocity,
+            targetVelocity,
+            1 - Mathf.Exp(-moveAcceleration * Time.deltaTime)
+        );
+
+        // Apply the velocity
+        transform.position += currentVelocity * Time.deltaTime;
     }
 
     void HeadTrackingUpdate()
---
+++ Assets/Scripts/GeckoLegStepper.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeckoLegStepper : MonoBehaviour
+{
+    // The position and rotation we want to stay in range of
+    [SerializeField] Transform homeTransform;
+    // Stay within this distance of home
+    [SerializeField] float wantStepAtDistance;
+    // How long a step takes to complete
+    [SerializeField] float moveDuration;
+    // Fraction of the max distance from home we want to overshoot by
+    [SerializeField] float stepOvershootFraction;
+    
+    // Is the leg moving?
+    public bool isMoving;
+
+    public void RequestMove()
+    {
+        // If we are already moving, don't start another move
+        if (isMoving) return;
+
+        float distFromHome = Vector3.Distance(transform.position, homeTransform.position);
+
+        // If we are too far off in position or rotation
+        if (distFromHome > wantStepAtDistance)
+        {
+            // Start the step coroutine
+            StartCoroutine(MoveToHome());
+        }
+    }
+
+    // Coroutines must return an IEnumerator
+    IEnumerator MoveToHome()
+    {
+        // Indicate we're moving (used later)
+        isMoving = true;
+
+        Vector3 startPoint = transform.position;
+        Quaternion startRot = transform.rotation;
+    
+        Quaternion endRot = homeTransform.rotation;
+    
+        // Directional vector from the foot to the home position
+        Vector3 towardHome = (homeTransform.position - transform.position);
+        // Total distance to overshoot by   
+        float overshootDistance = wantStepAtDistance * stepOvershootFraction;
+        Vector3 overshootVector = towardHome * overshootDistance;
+        // Since we don't ground the point in this simplified implementation,
+        // we restrict the overshoot vector to be level with the ground
+        // by projecting it on the world XZ plane.
+        overshootVector = Vector3.ProjectOnPlane(overshootVector, Vector3.up);
+
+        // Apply the overshoot
+        Vector3 endPoint = homeTransform.position + overshootVector;
+    
+        // We want to pass through the center point
+        Vector3 centerPoint = (startPoint + endPoint) / 2;
+        // But also lift off, so we move it up by half the step distance (arbitrarily)
+        centerPoint += homeTransform.up * Vector3.Distance(startPoint, endPoint) / 2f;
+
+        float timeElapsed = 0;
+        do
+        {
+            timeElapsed += Time.deltaTime;
+
+            float normalizedTime = timeElapsed / moveDuration;
+            normalizedTime = Easing.Cubic.InOut(normalizedTime);
+
+            // Quadratic bezier curve
+            transform.position =
+                Vector3.Lerp(
+                    Vector3.Lerp(startPoint, centerPoint, normalizedTime),
+                    Vector3.Lerp(centerPoint, endPoint, normalizedTime),
+                    normalizedTime
+                );
+
+            transform.rotation = Quaternion.Slerp(startRot, endRot, normalizedTime);
+
+            yield return null;
+        }
+        while (timeElapsed < moveDuration);
+
+        isMoving = false;
+    }
+}
